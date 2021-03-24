f8001d83e1f2d842882730c19042c564363cb8da
Wil Thieme <wil.thieme@protonmail.com>
2021-03-24 11:14:47 -0400

    functionalize

--- scripts/coinweight.lua
+++ scripts/coinweight.lua
@@ -28,54 +28,64 @@ function onInit()
 		aDenominations['cp'] = { ['nValue'] = .01, ['nWeight'] = .02 }
 	end
 end
+
+-- upgrade method to support removing second coins column - probably not needed anymore
+local function upgradeDamnedCoinWeight(nodeCoinSlot)
+	if DB.getValue(nodeCoinSlot, 'amountA') and DB.getValue(nodeCoinSlot, 'amountA', 0) ~= 0 then
+		nCoinAmount = nCoinAmount + DB.getValue(nodeCoinSlot, 'amountA', 0)
+		DB.setValue(nodeCoinSlot, 'amount', 'number', nCoinAmount)
+		if DB.getValue(nodeCoinSlot, 'amountA') then nodeCoinSlot.getChild('amountA').delete() end
+	end
+end
+
+local function createCoinsItem(nodeChar)
+	local nodeCoinsItem = DB.createChild(nodeChar.getChild('inventorylist'))
+	DB.setValue(nodeCoinsItem, 'name', 'string', 'Coins')
+	DB.setValue(nodeCoinsItem, 'type', 'string', 'Wealth and Money')
+	DB.setValue(nodeCoinsItem, 'description', 'string', Interface.getString("item_description_coins"))
+	
+	return nodeCoinsItem
+end
 	
 ---	Calculate weight of all coins and total value (in gp).
 --	@param nodeChar databasenode of PC within charsheet
 local function computeCoins(nodeChar)
-	local nTotalCoinsWeight = 0
-	local nWealth = 0
+	local nTotalCoinsWeight, nTotalCoinsWealth = 0, 0
 
 	for _,nodeCoinSlot in pairs(DB.getChildren(nodeChar, 'coins')) do
 		local sDenomination = string.lower(DB.getValue(nodeCoinSlot, 'name', ''))
 		local nCoinAmount = DB.getValue(nodeCoinSlot, 'amount', 0)
 
 		-- upgrade method to support removing second coins column - probably not needed anymore
-		if DB.getValue(nodeCoinSlot, 'amountA') and DB.getValue(nodeCoinSlot, 'amountA', 0) ~= 0 then
-			nCoinAmount = nCoinAmount + DB.getValue(nodeCoinSlot, 'amountA', 0)
-			DB.setValue(nodeCoinSlot, 'amount', 'number', nCoinAmount)
-			if DB.getValue(nodeCoinSlot, 'amountA') then nodeCoinSlot.getChild('amountA').delete() end
-		end
+		upgradeDamnedCoinWeight(nodeCoinSlot)
 
 		if sDenomination ~= '' then
 			for sDenominationName,tDenominationData in pairs(aDenominations) do
 				if string.match(sDenomination, string.lower(sDenominationName)) then
-					nWealth = nWealth + (nCoinAmount * tDenominationData['nValue'])
-					nTotalCoinsWeight = nTotalCoinsWeight + (nCoinAmount * tDenominationData['nWeight'])
+					nTotalCoinsWealth = nTotalCoinsWealth + (nCoinAmount * tDenominationData['nValue'])
+					nTotalCoinsWeight = math.floor(nTotalCoinsWeight + (nCoinAmount * tDenominationData['nWeight']))
 				end
 			end
 		end
 	end
 
+	local nodeCoinsItem
+	for _,nodeItem in pairs(DB.getChildren(nodeChar, 'inventorylist')) do
+		local sItemName = DB.getValue(nodeItem, 'name', '')
+		if sItemName == 'Coins' then
+			nodeCoinsItem = nodeItem
+		end
+	end
 	if nTotalCoinsWeight then
-		local nTotalCoinWeightToSet = math.floor(nTotalCoinsWeight)
-		local nodeOtherCoins
-		for _,nodeItem in pairs(DB.getChildren(nodeChar, 'inventorylist')) do
-			local sItemName = DB.getValue(nodeItem, 'name', '')
-			if sItemName == 'Coins' then
-				nodeOtherCoins = nodeItem
-			end
-		end
-		if nTotalCoinWeightToSet > 0 and not nodeOtherCoins then
-			nodeOtherCoins = DB.createChild(nodeChar.getChild('inventorylist'))
-		end
-		if nTotalCoinWeightToSet == 0 and nodeOtherCoins then
-			nodeOtherCoins.delete()
-		elseif nodeOtherCoins then
-			DB.setValue(nodeOtherCoins, 'name', 'string', 'Coins')
-			DB.setValue(nodeOtherCoins, 'type', 'string', 'Wealth and Money')
-			DB.setValue(nodeOtherCoins, 'cost', 'string', nWealth .. ' gp')
-			DB.setValue(nodeOtherCoins, 'description', 'formattedtext', Interface.getString("item_description_coins"))
-			DB.setValue(nodeOtherCoins, 'weight', 'number', nTotalCoinWeightToSet)
+		if nTotalCoinsWeight ~= 0 and not nodeCoinsItem then
+			nodeCoinsItem = createCoinsItem(nodeChar)
+		elseif nTotalCoinsWeight == 0 and nodeCoinsItem then
+			nodeCoinsItem.delete()
+		elseif nTotalCoinsWeight < 0 and nodeCoinsItem then
+			DB.setValue(nodeCoinsItem, 'cost', 'string', nTotalCoinsWealth .. ' gp')
+		elseif nodeCoinsItem then
+			DB.setValue(nodeCoinsItem, 'cost', 'string', nTotalCoinsWealth .. ' gp')
+			DB.setValue(nodeCoinsItem, 'weight', 'number', nTotalCoinsWeight)
 		end
 	end
 end
