2a116fbd4519764ff643a0faaa6dbc1e0be27b4c
Wil Thieme <wil.thieme@protonmail.com>
2021-05-18 07:56:45 -0400

    more functionalization

--- scripts/coinweight.lua
+++ scripts/coinweight.lua
@@ -45,17 +45,6 @@ local function upgradeDamnedCoinWeight(nodeCoinSlot)
 	end
 end
 
---	This function creates the "Coins" item in a PC's inventory.
---	It populates the name, type, and description and then returns the database node.
-local function createCoinsItem(nodeChar)
-	local nodeCoinsItem = DB.createChild(nodeChar.getChild('inventorylist'))
-	DB.setValue(nodeCoinsItem, 'name', 'string', 'Coins')
-	DB.setValue(nodeCoinsItem, 'type', 'string', 'Wealth and Money')
-	DB.setValue(nodeCoinsItem, 'description', 'formattedtext', Interface.getString("item_description_coins"))
-	
-	return nodeCoinsItem
-end
-
 ---	This function rounds to the specified number of decimals
 local function round(number, decimals)
     local n = 10^(decimals or 0)
@@ -65,11 +54,11 @@ local function round(number, decimals)
 end
 
 --- This function figures out how many decimal places to round to.
---	If the total weight is greater than or equal to 10, it rounds to 1 decimal place.
---	If it's greater than or equal to 100, it rounds to whole numbers.
---	If it's less than 10, it rounds to 2 decimal places.
+--	If the total weight is greater than or equal to 100, it recommends 0 (whole numbers).
+--	If it's greater than or equal to 10, it recommends 1.
+--	If it's greater than or equal to 1, it recommends 2.
 --	This maximizes difficulty at low levels when it has the most impact.
---	At higher weights, the intent is to keep the data visible on the inventory list.
+--	The intent is to keep the number visible on the inventory list without clipping.
 local function determineRounding(nTotalCoinsWealth)
 	if nTotalCoinsWeight >= 100 then
 		return 0
@@ -78,6 +67,17 @@ local function determineRounding(nTotalCoinsWealth)
 	else
 		return 2
 	end 
+end
+
+--	This function creates the "Coins" item in a PC's inventory.
+--	It populates the name, type, and description and then returns the database node.
+local function createCoinsItem(nodeChar)
+	local nodeCoinsItem = DB.createChild(nodeChar.getChild('inventorylist'))
+	DB.setValue(nodeCoinsItem, 'name', 'string', 'Coins')
+	DB.setValue(nodeCoinsItem, 'type', 'string', 'Wealth and Money')
+	DB.setValue(nodeCoinsItem, 'description', 'formattedtext', Interface.getString("item_description_coins"))
+	
+	return nodeCoinsItem
 end
 
 ---	This function looks for the "Coins" inventory item if it already exists.
@@ -90,15 +90,29 @@ local function findCoinsItem(nodeChar)
 	end
 end
 
+---	This function writes the coin data to the database.
+local function writeCoinData(nodeChar, nTotalCoinsWeight, nTotalCoinsWealth)
+	local nodeCoinsItem = findCoinsItem(nodeChar)
+	if (nTotalCoinsWeight > 0 or nTotalCoinsWealth ~= 0) and not nodeCoinsItem then
+		nodeCoinsItem = createCoinsItem(nodeChar)
+	end
+	if (nTotalCoinsWeight <= 0 and nTotalCoinsWealth == 0) and nodeCoinsItem then
+		nodeCoinsItem.delete()
+	elseif nTotalCoinsWeight < 0 and nodeCoinsItem then
+		DB.setValue(nodeCoinsItem, 'cost', 'string', nTotalCoinsWealth .. ' gp')
+		DB.setValue(nodeCoinsItem, 'weight', 'number', 0) -- coins can't be negative weight
+	elseif nodeCoinsItem then
+		DB.setValue(nodeCoinsItem, 'cost', 'string', nTotalCoinsWealth .. ' gp')
+		DB.setValue(nodeCoinsItem, 'weight', 'number', round(nTotalCoinsWeight, determineRounding(nTotalCoinsWealth)))
+	end
+end
+
 ---	This function calculates the weight of all coins and their total value (in gp).
 --	It looks at each coins database subnode and checks them for the data of other extensions.
 --	Then, it checks their denominations agains those defined in aDenominations.
 --	If it doesn't find a match, it assumes a coin weight of .02.
---	It then looks for an item in the inventory called "Coins"
---	If it doesn't find it, and the weight or value total is not zero, it creates it.
 local function computeCoins(nodeChar)
 	local nTotalCoinsWeight, nTotalCoinsWealth = 0, 0
-
 	for _,nodeCoinSlot in pairs(DB.getChildren(nodeChar, 'coins')) do
 		-- import data from other extensions
 		upgradeDamnedCoinWeight(nodeCoinSlot)
@@ -116,20 +130,7 @@ local function computeCoins(nodeChar)
 			nTotalCoinsWeight = nTotalCoinsWeight + (nCoinAmount * .02)
 		end
 	end
-
-	local nodeCoinsItem = findCoinsItem(nodeChar)
-	if (nTotalCoinsWeight > 0 or nTotalCoinsWealth ~= 0) and not nodeCoinsItem then
-		nodeCoinsItem = createCoinsItem(nodeChar)
-	end
-	if (nTotalCoinsWeight <= 0 and nTotalCoinsWealth == 0) and nodeCoinsItem then
-		nodeCoinsItem.delete()
-	elseif nTotalCoinsWeight < 0 and nodeCoinsItem then
-		DB.setValue(nodeCoinsItem, 'cost', 'string', nTotalCoinsWealth .. ' gp')
-		DB.setValue(nodeCoinsItem, 'weight', 'number', 0) -- coins can't be negative weight
-	elseif nodeCoinsItem then
-		DB.setValue(nodeCoinsItem, 'cost', 'string', nTotalCoinsWealth .. ' gp')
-		DB.setValue(nodeCoinsItem, 'weight', 'number', round(nTotalCoinsWeight, determineRounding(nTotalCoinsWealth)))
-	end
+	writeCoinData(nodeChar, nTotalCoinsWeight, nTotalCoinsWealth)
 end
 
 --	This function is called when a coin field is changed
